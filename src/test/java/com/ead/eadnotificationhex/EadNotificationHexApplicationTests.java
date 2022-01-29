8addd355f33245223080bcd16da82e9e29cfe6e7
Marcos Vassler <marcosvassoler@finchsolucoes.com.br>
2022-01-29 11:32:44 -0300

    Initial commit

---
+++ src/test/java/com/ead/eadnotificationhex/EadNotificationHexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.eadnotificationhex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadNotificationHexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
